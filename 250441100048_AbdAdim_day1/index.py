56042d7c0f5de3ecc69c3ae559cce05fb7ed92e0
adim619 <abduladim0986@gmail.com>
2025-09-13 11:46:52 +0700

    250441100048_AbdAdim_day1

---
+++ 250441100048_AbdAdim_day1/index.py
@@ -0,0 +1,29 @@
+# nama = "abd adim"
+# nim = 250441100048
+# semester = 1
+
+# print("======BIODATA MAHASISWA======")
+# print("halo perkenalkan nama saya: " + nama)
+# print(f"nim saya: {nim}")
+# print("saya saat ini semester: {}".format(semester))
+
+# a = 10
+# b = 5
+# c = a + b #penjumlahan
+# d = a - b #pengurangan
+# e = a * b #perkalian
+# f = a / b #pembagian
+# g = a % b #sisa bagi
+# print("hasil penjumlahan", c)
+# print("hasil pengurangan", d)
+# print("hasil perkalian", e)
+# print("hasil pembangian", f)
+# print("hasil sisa bagi", g)
+
+nama = input("masukkan nama anda: ")
+umur = input("masukkan umur anda:")
+buah = input("masukkan buah favorit anda:")
+
+print("nama saya adalah " + nama)
+print("umur saya adalah " + umur  )
+print("buat favorit saya adalah" + buah)
